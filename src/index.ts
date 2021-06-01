0709bb5c12a84cfc4724704223dc933a9324bc1c
Benjamin Ashbaugh <hi@benjaminashbaugh.me>
2021-05-31 23:46:41 -0600

    need express listener :facepalm:

--- src/index.ts
+++ src/index.ts
@@ -16,7 +16,9 @@ registerBotListeners(bot)
 registerHNWebhookListeners(router)
 
 async function start() {
-  await bot.start(parseInt(process.env.PORT) || 5000)
+  const port = parseInt(process.env.PORT) || 5000
+  router.listen(port)
+  await bot.start(port)
   console.log('⚡️ Bot started')
 }
 
