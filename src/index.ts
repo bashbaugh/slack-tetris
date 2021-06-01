aa01eff8df6630531bc902c0fd22654786d58c1f
Benjamin Ashbaugh <hi@benjaminashbaugh.me>
2021-05-31 23:29:53 -0600

    enable receiver

--- src/index.ts
+++ src/index.ts
@@ -8,7 +8,8 @@ const receiver = new ExpressReceiver({ signingSecret: process.env.SLACK_SIGNING_
 export const bot = new App({
   token: process.env.SLACK_BOT_TOKEN,
   appToken: process.env.SLACK_APP_TOKEN,
-  socketMode: true
+  socketMode: true,
+  receiver
 })
 
 registerBotListeners(bot)
